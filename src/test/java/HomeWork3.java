008f1eccaf720435f69a1de3a8d5a6598515fcdc
TTopuria <TTopuria@DESKTOP-V1LF9O0>
2022-11-20 00:53:43 +0400

    Default Commit

--- src/test/java/HomeWork3.java
+++ src/test/java/HomeWork3.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class HomeWork3 {
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeWork3 {
+    String BASEURL = "https://www.google.com/";
+    String searchLabelName = "q";
+    String searchButtonName = "btnK";
+
+    // String imageHyperLinkedTex = "სურათები";
+
+    @Test
+    public void googleTest(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+
+        chromeDriver.get(BASEURL);
+        chromeDriver.findElement(By.name(searchLabelName)).sendKeys("Tato Topuria");
+        chromeDriver.findElement(By.name(searchButtonName)).click();
+
+        chromeDriver.close();
+    }
 }
